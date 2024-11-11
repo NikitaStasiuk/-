e73f4db46b1786d1f8976f8bb0cb590fc1ae9846
NikitaStasiuk <n.stasyuk@e-u.edu.ua>
2024-11-11 20:06:07 +0200

    rust_repository

---
+++ main.rs
@@ -0,0 +1,21 @@
+mod chapter3;
+mod chapter4;
+mod chapter5;
+mod chapter6;
+mod practice_4;
+mod practice_5;
+mod practice_6;
+mod practice_7;
+mod practice_8;
+mod practice_9;
+mod practice_10;
+mod practice_11;
+mod practice_12;
+mod practice_13;
+mod practice_14;
+mod practice_15;
+mod practice_16;
+
+fn main() {
+    println!("Hello, world!");
+}
